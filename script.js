56938101ceb77d6d550365a1d8f0bdd0ee1626bc
CograndoIris <cgrandorf@irishealthcare.com>
2025-04-23 14:56:57 -0500

    Hidden & Output
    Hidden & Output functionality added but not yet complete throughout

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+const outputState = {
+    contact: '',
+    docsReceived: ''
+};
 
 
-const showVoiceMail = () => {
-    const voiceMail = document.querySelector('#voice-mail');
-    voiceMail.classList.style.display = "";
+const updateOutput = () => {
+    const outputText = `${outputState.contact}
+${outputState.docsReceived}`.trim();
+    document.querySelector('#output').textContent = outputText;
+}
+const hideClass = (inclass) => {
+    const elements = document.querySelectorAll(`.${inclass}`);
+    elements.forEach((element) => {
+        element.classList.add("hidden");
+    });
+}
+const showClass = (inclass) => {
+    const elements = document.querySelectorAll(`.${inclass}`);
+    elements.forEach((element) => {
+        element.classList.remove("hidden");
+    });
 }
 
-const hideVoiceMail = () => {
-    const voiceMail = document.querySelector('#voice-mail');
-    voiceMail.classList.style.display = "none";
-}
-
-const contactedEvent = document.querySelector('#contact');
-contactedEvent.addEventListener('click', (event) => {
-    const voiceMail = document.querySelector('#voice-mail');
-    if (voiceMail.classList.value('contact') === 'No') {
-        showVoiceMail();
-    } else { 
-        hideVoiceMail();
-    }
+const contactedEvent = document.querySelectorAll('.contactbutton');
+contactedEvent.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        let output = event.currentTarget.value;
+        outputState.contact = `Contacted: ${output}`;
+        if (output === 'No') {
+            showClass('voicemail-form');
+        } 
+        if (event.currentTarget.value === 'Yes') { 
+            hideClass('voicemail-form');
+        }
+        updateOutput();
+    });
 });
+
+//Need Logic for Spoke With
+
+const docsreceivedEvent = document.querySelectorAll('.docsreceivedbutton');
+docsreceivedEvent.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        let output = event.currentTarget.value;
+        outputState.docsReceived = `Docs Received: ${output}`;
+        if (output === 'No') {
+            showClass('docscreated-form');
+        } 
+        if (event.currentTarget.value === 'Yes') { 
+            hideClass('docscreated-form');
+        }
+        updateOutput();
+    });
+});
+
+//const output = "hi there";
+//document.querySelector('#output').textContent = output;
+//document.querySelector('#output').textContent = contactoutput;
